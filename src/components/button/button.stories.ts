d03d3fc16761863688a695c677a3f688553bf7b8
SHIMADA Takayuki <takayshim@digital.go.jp>
2025-05-27 14:08:55 +0900

    refactor(button): 不要なコード削除

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -15,7 +15,6 @@ interface ButtonProps {
   label: string;
 }
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Components/ボタン",
   render: (args) => {
