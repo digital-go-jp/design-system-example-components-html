433ae24e072137b4a29fff8b5d69cda6c97d460c
SHIMADA Takayuki <takayshim@digital.go.jp>
2025-05-28 10:43:45 +0900

    feat(breadcrumb): パンくずリストを追加

---
+++ src/components/breadcrumb/breadcrumb.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/html";
+import { HtmlFragment } from "../../helpers/html-fragment";
+
+import "./breadcrumb.css";
+import plain from "./plain.html?raw";
+import withVisibleLabel from "./with-visible-label.html?raw";
+import withHomeIcon from "./with-home-icon.html?raw";
+
+const meta = {
+  title: "Components/パンくずリスト",
+  render: () => {
+    const fragment = new HtmlFragment(plain, ".dads-breadcrumb");
+    return fragment.toString();
+  },
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj;
+
+export const Plain: Story = {};
+
+export const WithVisibleLabel = () =>
+  new HtmlFragment(withVisibleLabel, ".dads-breadcrumb").toString();
+
+export const WithHomeIcon = () =>
+  new HtmlFragment(withHomeIcon, ".dads-breadcrumb").toString();
