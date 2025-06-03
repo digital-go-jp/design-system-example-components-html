6e3c2dd9e6d61cc9d60408560a634f4192ac4885
SHIMADA Takayuki <takayshim@digital.go.jp>
2025-06-03 15:45:12 +0900

    feat(date-picker): 日付ピッカー（Consolidated）を追加

--- playwright.config.js
+++ playwright.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./src",
+  use: {
+    launchOptions: {
+      args: ["--disable-web-security"],
+    },
+  },
 });
