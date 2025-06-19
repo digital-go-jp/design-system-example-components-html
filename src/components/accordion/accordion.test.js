5f3f8eaa3cf76cc1fba189c0ea46ee3483fdd54f
SHIMADA Takayuki <takayshim@digital.go.jp>
2025-06-19 14:46:35 +0900

    test: 無視する要素を指定できるようにする

--- src/components/accordion/accordion.test.js
+++ src/components/accordion/accordion.test.js
@@ -3,4 +3,6 @@ import { resetCssVrt } from "../../../tests/helpers/reset-css-vrt";
 
 const { dirname } = import.meta;
 
-resetCssVrt("stacked", path.join(dirname, "stacked.html"));
+resetCssVrt("stacked", path.join(dirname, "stacked.html"), {
+  ignoreElements: [".dads-accordion__content"],
+});
--- src/components/list/list.test.js
+++
@@ -1,6 +0,0 @@
-import path from "node:path";
-import { resetCssVrt } from "../../../tests/helpers/reset-css-vrt";
-
-const { dirname } = import.meta;
-
-resetCssVrt("list-all", path.join(dirname, "all-lists.html"));
