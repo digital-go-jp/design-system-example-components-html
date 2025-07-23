d3c01f59c88eb2c6af57bbf699bc7314327af2b4
SHIMADA Takayuki <takayshim@digital.go.jp>
2025-07-23 09:16:01 +0900

    test(link): リンクのテストはいったん落とす

--- src/components/link/link.test.js
+++
@@ -1,6 +0,0 @@
-import path from "node:path";
-import { resetCssVrt } from "../../../tests/helpers/reset-css-vrt";
-
-const { dirname } = import.meta;
-
-resetCssVrt("playground", path.join(dirname, "playground.html"));
