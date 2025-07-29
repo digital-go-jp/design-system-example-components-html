0045992b9d1f9f5697f31af60b61998f75e93278
SHIMADA Takayuki <takayshim@digital.go.jp>
2025-07-29 09:58:10 +0900

    feat(notification-banner): モバイル条件付きボタンの適用例を追加

--- src/components/notification-banner/notification-banner.test.js
+++ src/components/notification-banner/notification-banner.test.js
@@ -4,3 +4,8 @@ import { resetCssVrt } from "../../../tests/helpers/reset-css-vrt";
 const { dirname } = import.meta;
 
 resetCssVrt("notification-banner-success", path.join(dirname, "success.html"));
+
+resetCssVrt(
+  "notification-banner-mobile-compact",
+  path.join(dirname, "mobile-compact.html"),
+);
