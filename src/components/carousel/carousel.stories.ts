3b943ef108d4c817245a14ae8d5f34ee5b5896a8
SHIMADA Takayuki <takayshim@digital.go.jp>
2026-02-04 11:55:27 +0900

    feat(carousel): 高解像度のスライド画像を使用する

--- src/components/carousel/carousel.stories.ts
+++ src/components/carousel/carousel.stories.ts
@@ -11,12 +11,32 @@ import image2 from "./image-2.webp";
 import image3 from "./image-3.webp";
 import image4 from "./image-4.webp";
 
+import image1_2x from "./image-1@2x.webp";
+import image2_2x from "./image-2@2x.webp";
+import image3_2x from "./image-3@2x.webp";
+import image4_2x from "./image-4@2x.webp";
+
 const images: Record<string, string> = {
   "image-1.webp": image1,
   "image-2.webp": image2,
   "image-3.webp": image3,
   "image-4.webp": image4,
 };
+
+const images2x: Record<string, string> = {
+  "image-1.webp": image1_2x,
+  "image-2.webp": image2_2x,
+  "image-3.webp": image3_2x,
+  "image-4.webp": image4_2x,
+};
+
+function updateImageSrcAndSrcset(img: Element) {
+  const src = img.getAttribute("src")?.match(/\/?([^/]+)$/)?.[1];
+  if (src && images[src]) {
+    img.setAttribute("src", images[src]);
+    img.setAttribute("srcset", `${images2x[src]} 2x`);
+  }
+}
 
 const meta = {
   title: "Components/カルーセル",
@@ -29,10 +49,7 @@ export const Normal: StoryObj = {
     const fragment = new HtmlFragment(normal, "dads-carousel");
 
     fragment.root.querySelectorAll("img").forEach((img) => {
-      const src = img.getAttribute("src")?.match(/\/?([^/]+)$/)?.[1];
-      if (src) {
-        img.setAttribute("src", images[src]);
-      }
+      updateImageSrcAndSrcset(img);
     });
 
     return fragment.toString();
